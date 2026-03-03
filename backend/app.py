31e50c201b1b9101a91b2f30fac5327b3799c187
Alma-Raveena <raveenaalma8@gmail.com>
2026-03-03 09:32:59 +0530

    Fix port for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -181,5 +181,8 @@ def summary_all_locations():
 # -----------------------------
 # Run app
 # -----------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
